306b0b00c34d7d2e71ff18e68db4f6131e8c763e
Ales Guzik <me@aguzik.net>
2013-11-23 17:17:28 +0300

    take work screenshots

--- back_to_work.sh
+++ back_to_work.sh
@@ -2,9 +2,13 @@
 DELAY=${1:-120}
 sleep $DELAY
 while true; do
-  zenity  --question \
+  if zenity  --question \
     --title "Back to work" \
-    --text "Is what you are doing now directly work-related?" ||
-  xscreensaver-command -lock
+    --text "Is what you are doing now directly work-related?"
+  then
+    screenlog take
+  else
+    xscreensaver-command -lock
+  fi
   sleep $((5*60))
 done
